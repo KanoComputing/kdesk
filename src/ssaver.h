7c76219d74f2691eb097ab5d88c14ce4da69d2da
Albert Casals <skarbat@gmail.com>
2014-05-16 08:09:50 -0400

    Adding 2 new icon hooks for screen saver start and finish

--- src/ssaver.h
+++ src/ssaver.h
@@ -7,9 +7,12 @@
 // An app to show and bring life to Kano-Make Desktop Icons.
 //
 
-#define POLL_INTERVAL    15000           // milliseconds between each system idle query
-#define XREFRESH         "xrefresh"      // called after the screen saver to redraw the desktop
-#define TTY_QUERY        "/dev/tty1"     // name of the tty device to use as a trampoline to know who has the focus
+#define POLL_INTERVAL      15000                // milliseconds between each system idle query
+#define XREFRESH           "xrefresh"           // called after the screen saver to redraw the desktop
+#define TTY_QUERY          "/dev/tty1"          // name of the tty device to use as a trampoline to know who has the focus
+#define SSAVER_HOOK_START  "ScreenSaverStart"   // First parameter name sent to hooks when the screen saver is about to start
+#define SSAVER_HOOK_FINISH "ScreenSaverFinish"  // First parameter name sent to hooks when screen saver terminates
+
 #define GUI_TTY_DEVICE   7               // tty device number where the GUI XServer is running, normally tty7
 
 typedef struct _ksaver_data {
@@ -17,6 +20,7 @@ typedef struct _ksaver_data {
   const char *display_name;           // Usually this can be set to NULL to attach to first available display
   unsigned long idle_timeout;         // seconds to idle before starting the screen saver
   const char *saver_program;          // path to binary program that paints the screen saver
+  const char *saver_hooks;            // path to a hook script that will be executed to alert on screen saver transitions (start, finish)
 
 } KSAVER_DATA;
 
@@ -24,3 +28,6 @@ typedef KSAVER_DATA* PKSAVER_DATA;
 
 bool setup_ssaver (KSAVER_DATA *kdata);
 void *idle_time (void *p);
+int hook_ssaver_start(const char *hook_script);
+int hook_ssaver_finish(const char *hook_script);
+void fake_user_input (void);
